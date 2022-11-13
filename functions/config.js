e25052588d9d745340dc6e7f98d171cef5ddd624
markbreuss <mark.breuss@markone.at>
2022-11-13 21:01:14 +0100

    added config js file

---
+++ functions/config.js
@@ -0,0 +1,8 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    location: process.env.LOCATION || "us-central1",
+    collectionPath: process.env.COLLECTION_PATH || "collectionName",
+    ProjectApiKey: process.env.PROJECT_API_KEY || "some_api_key",
+};
