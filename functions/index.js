171f29a8c7280cb9276d899054e52c257130c906
markbreuss <mark.breuss@markone.at>
2022-11-26 15:36:07 +0100

    added new backend api route

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,32 @@
 /*
-
+handles firestore document updates and sends them the the firea backend
 */
 
+const functions = require('firebase-functions');
+const axios = require('axios')
+const config = require("./config");
 
-import { firestore } from "firebase-functions";
 
-export const fsStreamCollection = firestore.document("users/{userId}").onWrite((change, context) => {
-      // If we set `/users/marie` to {name: "Marie"} then
-      // context.params.userId == "marie"
-      // change.after.data() == {name: "Marie"}
-      console.log('hello function');
-      console.log(change);
-      console.log(context);
+exports.fsStreamCollection = functions.handler.firestore.document.onWrite((change, context) => {
+    functions.logger.log('Sync Collection');
+
+      //payload sent to the firea backend
+      const data = {
+        'collection_name':config.default.collectionPath,
+        'doc':change.after.data(),
+        'api_key':config.default.ProjectApiKey,
+        'doc_id':change.after.id,
+      };
+      
+      //Enpoint of the firea backend data server 
+      //todo route based on location server
+      //const dataEndpoint = 'https://qa-firea.anvil.app/_/api/data';
+      const dataEndpoint = "https://eu-central-1.aws.data.mongodb-api.com/app/firea_main-lxgkm/endpoint/update"
+
+      axios.post(dataEndpoint, data).then(res => {
+          functions.logger.log('STATUS',res.status);
+        })
+        .catch(err => {
+          functions.logger.log('Error',err);
+        })
     });
