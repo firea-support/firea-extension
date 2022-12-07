f1cd163cd6b199c13fef07c2b1e1cc29e47797b9
markbreuss <mark.breuss@markone.at>
2022-12-07 13:29:21 +0100

    new delete doc method

--- functions/index.js
+++ functions/index.js
@@ -9,9 +9,10 @@ const extensionConfig = require("./config");
 
 exports.fsStreamCollection = functions.handler.firestore.document.onWrite((change, context) => {
     functions.logger.log('Sync Collection');
+    
     //check if doc was deleted
     const isDocDeleted = !change.after.exists;
-    const docRef = change.after.ref.collectionPath();
+    const docRef = change.after.ref.path;
 
       //header for sending to firea backend
       let requestOptions = {
@@ -19,17 +20,20 @@ exports.fsStreamCollection = functions.handler.firestore.document.onWrite((chang
           "X-Firea-Api-Key": extensionConfig.default.projectApiKey,
           "X-Firea-Project-Id":extensionConfig.default.fireaProjectId,
           "X-Firea-Collection-Id":extensionConfig.default.collectionPath,
-          "X-Firea-DeleteDoc":isDocDeleted,
           "X-Firea-DocPath":docRef,
         }
       }
       
 
       //payload sent to the firea backend 
-      const data = change.after.data();
-      data['_id'] = change.after.id;
+      var data = {}
+      if (isDocDeleted){
+        data = {'__DELETE_DOCUMENT__':change.after.id}
+      } else {
+        data = change.after.data();
+        data['_id'] = change.after.id;
+      }
 
-      
       //Enpoint of the firea backend data server 
       //todo route data based on location to the nearest firea server
       const dataEndpoint = "https://update-a6smmjqo7a-uc.a.run.app/update";
